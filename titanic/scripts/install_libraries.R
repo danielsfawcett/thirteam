41ef360526330502e283fc501ab3fb065693b81c
Daniel <daniel@Daniels-MacBook-Pro.local>
2015-11-21 04:30:36 +0000

    Added install_libraries script

---
+++ titanic/scripts/install_libraries.R
@@ -0,0 +1,25 @@
+## Make sure the required libraries are installed and loaded
+
+## rattle
+if(!require('rattle')){
+  install.packages('rattle')
+  library('rattle')
+}
+
+## rpart
+if(!require('rpart')){
+  install.packages('rpart')
+  library('rpart')
+}
+
+## rpart.plot
+if(!require('rpart.plot')){
+  install.packages('rpart.plot')
+  library('rpart.plot')
+}
+
+## RColorBrewer
+if(!require('RColorBrewer')){
+  install.packages('RColorBrewer')
+  library('RColorBrewer')
+}
