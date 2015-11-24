3900a7c982a930fc7109a025177efef2c1e4b1b1
Daniel <daniel@Daniels-MacBook-Pro.local>
2015-11-24 14:41:56 +0000

    Added Nathan's age prediction and banding files
    Two scripts for engineering age and submission 5.

---
+++ titanic/scripts/band_age_C_YC_A_YA.R
@@ -0,0 +1,26 @@
+## Script to create bands of ages (Child 16 and under, everyone else Adult, inc missing age values)
+
+# add the survived column to the test data
+test$Survived <- NA
+
+# create a new data.frame that combines both train and test
+combi <- rbind(train, test)
+
+# assign Adult to all passengers (inc missing values in Age)
+combi$AgeCategory <- "Adult"
+
+# find passengers, banding the ages to four categories
+combi$AgeCategory[combi$Age <= 4] <- "YoungChild"
+combi$AgeCategory[combi$Age <= 16 & combi$Age > 4] <- "Child"
+combi$AgeCategory[combi$Age <= 25 & combi$Age > 16] <- "YoungAdult"
+combi$AgeCategory[combi$Age > 25] <- "Adult"
+combi$AgeCategory[is.na(combi$Age)] <- "NA"
+
+# split the data back into train and test
+train <- combi[1:891,]
+test <- combi[892:1309,]
+
+# make sure the AgeCategory columns are factors in both sets
+train$AgeCategory <- factor(train$AgeCategory)
+test$AgeCategory <- factor(test$AgeCategory)
+
