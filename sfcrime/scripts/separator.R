026ae54b7c099090c44a93508ceec0db64888d37
ralz29 <tsanova.ralitsa@yahoo.co.uk>
2015-11-27 09:52:05 +0000

    fixed separator

--- sfcrime/scripts/separator.R
+++ sfcrime/scripts/separator.R
@@ -3,6 +3,5 @@ test <- read.csv("test.csv", stringsAsFactors=FALSE)
 
 comb<-data.frame(Dates=c(train$Dates, test$Dates), Address=c(train$Address, test$Address), DayOfWeek=c(train$DayOfWeek, test$DayOfWeek), PDDistrict=c(train$PdDistrict, test$PdDistrict))
 
-
-comb$Time<-sapply(comb$Dates, FUN=function(x){strsplit(x, split=" ")[[1]][2]})
-comb$Dates<-sapply(comb$Dates, FUN=function(x){strsplit(x, split=' ')[[1]][1]})
+comb$Time<-sapply(as.character(comb$Dates), FUN=function(x){strsplit(x, split=' ')[[1]][2]})
+comb$Dates<-sapply(as.character(comb$Dates), FUN=function(x){strsplit(x, split=' ')[[1]][1]})
